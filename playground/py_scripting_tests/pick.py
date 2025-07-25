ca2c1056cd055889d17ae636cfee989ced63f9bf
Manillin <chri.vwm@gmail.com>
2025-07-25 18:23:32 +0200

    k  frequent test

--- playground/py_scripting_tests/pick.py
+++ playground/py_scripting_tests/pick.py
@@ -1,7 +1,23 @@
-import random
+from collections import defaultdict
+nums = [1, 1, 1, 2, 2, 3]
+nums2 = [1]
 
-index = random.randint(0, 1)
 
-choice = ['birra', 'biliardo']
+def kfrequent(nums, k):
+    result = []
+    num_map = defaultdict(int)
+    buckets = [[] for i in range(len(nums))]
+    for number in nums:
+        num_map[number] += 1
 
-print(f"Stasera -> {choice[index]}")
+    for key, value in num_map.items():
+        buckets[value].append(key)
+
+    for bucket in buckets[::-1]:
+        for number in bucket:
+            result.append(number)
+            if len(result) == k:
+                return result
+
+
+print(kfrequent(nums2, 2))
