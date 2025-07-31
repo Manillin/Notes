825793f0032877f1c390216b0d7cf9fb11f59a3d
Manillin <chri.vwm@gmail.com>
2025-07-31 16:53:17 +0200

    3sum solution

--- playground/py_scripting_tests/pick.py
+++ playground/py_scripting_tests/pick.py
@@ -1,32 +1,33 @@
+def threeSum(nums):
+    nums.sort()
+    triplets = []
 
-def encode(strs):
-    compressed = ''
-    for string in strs:
-        length = len(string)
-        compressed += '#'+str(length)+string
-    return compressed
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+
+        l, r = i+1, len(nums)-1
+        while l < r:
+            threeSum = nums[i] + nums[l] + nums[r]
+            if threeSum > 0:
+                r -= 1
+            elif threeSum < 0:
+                l += 1
+            else:
+                triplets.append([nums[i], nums[l], nums[r]])
+                l += 1
+                while l < r and nums[l] == nums[l-1]:
+                    l += 1
+    return triplets
 
 
-def decode(s: str):
-    strings = []
-    i = 0
-    while i < len(s):
-        if s[i] == '#':
-            j = i+1
-            start = j + 1
-            end = int(s[j]) + 1
-            substring = s[start:j+end]
-            print(substring)
-            strings.append(substring)
-            i = j + end
+def twoSum(numbers, target):
+    l, r = 0, len(numbers)-1
+    while l < r:
+        twosum = numbers[l] + numbers[r]
+        if twosum > target:
+            r -= 1
+        elif twosum < target:
+            l += 1
         else:
-            return strings
-    return strings
-
-
-strs = ["we", "say", ":", "yes", "!@#$%^&*()"]
-
-compressed = encode(strs)
-print(compressed)
-
-strings = decode(compressed)
+            return [l+1, r+1]
